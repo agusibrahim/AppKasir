285a601d8bcf54347812913ad5146d6a503a606a
Agus Ibrahim <mynameisagoes@facebook.com>
2016-09-27 07:05:14 +0700

    init project

---
+++ AppKasir/app/src/main/java/com/agusibrahim/appkasir/MainActivity.java
@@ -0,0 +1,14 @@
+package com.agusibrahim.appkasir;
+
+import android.app.*;
+import android.os.*;
+
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
